d5894bd69c38bb12162e18795a29e92490d1299c
RamcharanRajpurohit <b23ci1032@iitj.ac.in>
2024-06-05 12:07:38 +0530

    first commit

--- first.js
+++ first.js
@@ -1,3 +1,4 @@
 console.log("hello first time in terminal")
 console.log("this is a chnage to see is i am able to show it on github")
+console.log("this is a chnage to see is i am able to show it on github")
 
