aee473e654b1576007f7d2025826ebf85b09ed15
RamcharanRajpurohit <b23ci1032@iitj.ac.in>
2024-06-06 11:29:30 +0530

    just trying to fix setup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/home',(req,res)=>{
-    res.send("<h1>teri maa ki chut Khushi</h1>")
+    res.send("<h1>teri maa ki chut Khushi   or to or eski maa ka bhosda </h1>")
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
